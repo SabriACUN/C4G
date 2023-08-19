39a000577b6ab95cb8f6e3c518b9194b0a498233
İsmail Emre <96350235+iEmreM@users.noreply.github.com>
2023-08-19 22:13:19 +0300

    Door System
    Door interaction added

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public GameObject doorRight;
+    public GameObject doorLeft;
+    private bool isDoorOpen = false;
+    private int doorAngle = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("FBI Open the Door");
+        isDoorOpen = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDoorOpen && doorAngle < 90)
+        {
+            doorRight.transform.Rotate(0, 1, 0);
+            doorLeft.transform.Rotate(0, -1, 0);
+            doorAngle++;
+        }
+    }
+}
