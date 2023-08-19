399a03e37aae4571bf6d82cd93ce24382b36289b
Mertcan <mgr5404@gmail.com>
2023-08-19 23:03:09 +0300

    axe anim

--- Assets/Scripts/attackController.cs
+++ Assets/Scripts/attackController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class attackController : MonoBehaviour
 {
     private Animator anim;
+    private bool canAttack = true;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -13,7 +14,29 @@ public class attackController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-            anim.SetTrigger("attack");
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canAttack)
+        {
+            canAttack = false;
+            anim.Play("firstAttack");
+            StartCoroutine(delayy());
+        }
+        else if(Input.GetKeyDown(KeyCode.Mouse0) && canAttack == false)
+        {
+            anim.Play("secondAttack");
+        }
+    }
+    IEnumerator  delayy()
+    {
+        yield return new WaitForSeconds(2f);
+        anim.Play("idle");
+        canAttack = true;
+        yield return null;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            Debug.Log("vurdum oni");
+        }
     }
 }
